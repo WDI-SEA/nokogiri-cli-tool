4b53cce36c5ab04aedfabec2d074406026eea344
brytree <brycebortree@gmail.com>
2016-05-05 16:22:26 -0700

    shitty data validation that's antidry

--- astrology.rb
+++ astrology.rb
@@ -1,29 +1,49 @@
 require 'rest-client'
 require 'nokogiri'
 
-#define getting data from - in this case LOTR works' tags from a fanfiction website
-url = 'http://www.astrology.com/horoscope/daily/' + sign +  '.html'
-puts "fetching data from #{url}"
-
-#request data
-response = RestClient.get(url)
-html = response.body
-
-#local info
-# html = File.open('LOTR.htm')
-
-#give to nokogiri
-data = Nokogiri::HTML(html)
-
-#define target
-selector = ".page-horoscope-text"
-
-#select data using CSS selector
-elements = data.css(selector)
-
-elements.each do |el|
-  puts el.text.strip
-end
 
 puts "What's your sign?"
-sign = gets.chomp
+sign = gets.chomp.downcase
+
+signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
+
+if sign != "aries" &&
+   sign != "taurus" &&
+   sign != "gemini" &&
+   sign != "cancer" &&
+   sign != "leo" &&
+   sign != "virgo" &&
+   sign != "libra" &&
+   sign != "scorpio" &&
+   sign != "sagittarius" &&
+   sign != "capricorn" &&
+   sign != "aquarius" &&
+   sign != "pisces"
+  puts "Unfortunately that's not a sign! try again with a sign!"
+else
+
+  #define getting data from - in this case LOTR works' tags from a fanfiction website
+  url = 'http://www.astrology.com/horoscope/daily/' + sign +  '.html'
+  puts "fetching data from #{url}"
+
+  #request data
+  response = RestClient.get(url)
+  html = response.body
+
+  #local info
+  # html = File.open('LOTR.htm')
+
+  #give to nokogiri
+  data = Nokogiri::HTML(html)
+
+  #define target
+  selector = ".page-horoscope-text"
+
+  #select data using CSS selector
+  elements = data.css(selector)
+
+  elements.each do |el|
+    puts el.text.strip
+  end
+end
+
