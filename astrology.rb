55a1840e728edaa51f97e145e78b4defcde2d75f
brytree <brycebortree@gmail.com>
2016-05-05 16:31:51 -0700

    made it a function that loops if you put the wrong word in

--- astrology.rb
+++ astrology.rb
@@ -1,49 +1,52 @@
 require 'rest-client'
 require 'nokogiri'
 
+def whats_your_sign 
+  puts "What's your sign?"
+  sign = gets.chomp.downcase
 
-puts "What's your sign?"
-sign = gets.chomp.downcase
+  signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
 
-signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
+  if sign != "aries" &&
+     sign != "taurus" &&
+     sign != "gemini" &&
+     sign != "cancer" &&
+     sign != "leo" &&
+     sign != "virgo" &&
+     sign != "libra" &&
+     sign != "scorpio" &&
+     sign != "sagittarius" &&
+     sign != "capricorn" &&
+     sign != "aquarius" &&
+     sign != "pisces"
+    puts "Unfortunately that's not a sign! Try again with a sign!"
+    whats_your_sign
+  else
 
-if sign != "aries" &&
-   sign != "taurus" &&
-   sign != "gemini" &&
-   sign != "cancer" &&
-   sign != "leo" &&
-   sign != "virgo" &&
-   sign != "libra" &&
-   sign != "scorpio" &&
-   sign != "sagittarius" &&
-   sign != "capricorn" &&
-   sign != "aquarius" &&
-   sign != "pisces"
-  puts "Unfortunately that's not a sign! try again with a sign!"
-else
+    #define getting data from - in this case LOTR works' tags from a fanfiction website
+    url = 'http://www.astrology.com/horoscope/daily/' + sign +  '.html'
+    puts "fetching data from #{url}"
 
-  #define getting data from - in this case LOTR works' tags from a fanfiction website
-  url = 'http://www.astrology.com/horoscope/daily/' + sign +  '.html'
-  puts "fetching data from #{url}"
+    #request data
+    response = RestClient.get(url)
+    html = response.body
 
-  #request data
-  response = RestClient.get(url)
-  html = response.body
+    #local info
+    # html = File.open('LOTR.htm')
 
-  #local info
-  # html = File.open('LOTR.htm')
+    #give to nokogiri
+    data = Nokogiri::HTML(html)
 
-  #give to nokogiri
-  data = Nokogiri::HTML(html)
+    #define target
+    selector = ".page-horoscope-text"
 
-  #define target
-  selector = ".page-horoscope-text"
+    #select data using CSS selector
+    elements = data.css(selector)
 
-  #select data using CSS selector
-  elements = data.css(selector)
-
-  elements.each do |el|
-    puts el.text.strip
+    elements.each do |el|
+      puts el.text.strip
+    end
   end
 end
 
+whats_your_sign
