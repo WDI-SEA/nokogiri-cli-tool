433730b6dac1223ac7e79454691725a6e6827dd6
brytree <brycebortree@gmail.com>
2016-05-06 09:39:06 -0700

    made lotr search any fandom on ao3

--- LOTR.rb
+++ LOTR.rb
@@ -1,26 +1,43 @@
 require 'rest-client'
 require 'nokogiri'
 
-#define getting data from - in this case LOTR works' tags from a fanfiction website
-url = 'http://archiveofourown.org/tags/The%20Lord%20of%20the%20Rings%20-%20J*d*%20R*d*%20R*d*%20Tolkien/works'
-puts "fetching data from #{url}"
+def get_tags
+  puts "What fandom do you want to find tags for?"
+  fandom = gets.chomp.gsub(" ", "%20")
+  /The%20Lord%20of%20the%20Rings%20-%20J*d*%20R*d*%20R*d*%20Tolkien/
 
-#request data
-response = RestClient.get(url)
-html = response.body
+  #define getting data from - in this case LOTR works' tags from a fanfiction website
+  url = "http://archiveofourown.org/tags/#{fandom}"
+  puts "fetching data from #{url}"
 
-#local info
-# html = File.open('LOTR.htm')
+  #request data
+  response = RestClient.get(url)
+  html = response.body
 
-#give to nokogiri
-data = Nokogiri::HTML(html)
+  #local info
+  # html = File.open('LOTR.htm')
 
-#define target
-selector = "ul.tags.commas"
+  #give to nokogiri
+  data = Nokogiri::HTML(html)
 
-#select data using CSS selector
-elements = data.css(selector)
+  #define target
+  selector = "ul.tags.commas"
 
-elements.each do |el|
-  puts el.text.strip
+  #select data using CSS selector
+  elements = data.css(selector)
+
+  elements.each do |el|
+    puts el.text.strip
+  end
+
+  puts "Want to search for anything else? (yes/no)"
+  yes_no = gets.chomp
+
+  if yes_no == "yes" || yes_no == "y"
+    get_tags
+  else
+    puts "Thanks for searching!"
+  end
 end
+
+get_tags
